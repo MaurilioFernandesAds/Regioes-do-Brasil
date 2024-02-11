e297bc26d13d74f8afd9d6bc5b0b63d0131e668a
MaurilioFernandesAds <MaurilioFernandes.Ads@gmail.com>
2024-02-11 19:50:15 -0300

    1

--- Arquivos do Projeto/Exercicio001.py
+++
@@ -1,3 +0,0 @@
-# Exercício Python #001 - Qual o seu nome?
-name= input('Digite é o seu nome: ')
-print(f'É um prazer te conhecer, {name}!')
--- Arquivos do Projeto/Exercicio010.py
+++
@@ -1,4 +0,0 @@
-# Exercício Python #010 - Conversão de moedas
-real = float(input('Quanto dinheiro você tem na carteira? R$'))
-dolar = real / 3.27
-print(f'Com R$ {real:.2f} você pode comprar US$ {dolar:.2f} dolares.')
